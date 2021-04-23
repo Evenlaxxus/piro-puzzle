7e4b30e217ecb0ec630396043d9f487e464e4201
Wojciech Lulek <festorius@gmail.com>
2021-04-23 23:41:06 +0200

    Added base finder

--- main.py
+++ main.py
@@ -1,27 +1,28 @@
 import math
 import os
 import sys
-import cv2 as cv
-import numpy as np
+import cv2
 
 
 def angle(points):
     ang = math.degrees(math.atan2(points[2][1] - points[1][1], points[2][0] - points[1][0]) - math.atan2(
         points[0][1] - points[1][1], points[0][0] - points[1][0]))
-    return ang + 360 if ang < 0 else ang
+
+    if ang < 0:
+        return points[0], points[1], points[2], ang + 360
+    return points[0], points[1], points[2], ang
 
 
 def shiftAngleArray(angles):
     minimum = 360
     minDiffSum = 360
     for i in range(-1, len(angles) - 1):
-        diff = 90 - angles[i]
+        diff = 90 - angles[i][3]
         if diff < 0:
             diff = 0 - diff
         if diff < minimum:
-            diff2 = 90 - angles[i + 1]
-            if 85 < angles[i + 1] < 95:
-                diff2 = 90 - angles[i + 1]
+            if 85 < angles[i + 1][3] < 95:
+                diff2 = 90 - angles[i + 1][3]
                 if diff2 < 0:
                     diff2 = 0 - diff2
                 diffSum = diff + diff2
@@ -31,21 +32,50 @@ def shiftAngleArray(angles):
     return angles[possibleBaseAngles:] + angles[:possibleBaseAngles]
 
 
+# def scale_to_one_size
+# def rotate(image, base_points, contours):
+#     p1, p2 = base_points
+#     start_point, end_point = p1[2], p2[0]
+#     angle = math.atan2((end_point[1] - start_point[1]), (end_point[0] - start_point[0])) * (180.0 / math.pi)
+#     angle %= 180.0
+#
+#     moments = cv2.moments(contours[0])
+#     center_x = int(moments["m10"] / moments["m00"])
+#     center_y = int(moments["m01"] / moments["m00"])
+#     rot_mat = cv2.getRotationMatrix2D((center_y, center_x), angle, 1.0)
+#
+#     height, width = image.shape
+#     res_img = cv2.warpAffine(image, rot_mat, (width, height), flags=cv2.INTER_LINEAR)
+#
+#
+#     contours, hierarchy = cv2.findContours(res_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+#     imgCopy = cv2.cvtColor(res_img.copy(), cv2.COLOR_GRAY2BGR)
+#
+#     epsilon = 0.005 * cv2.arcLength(contours[0], True)
+#     approx = cv2.approxPolyDP(contours[0], epsilon, True)
+#
+#     cv2.drawContours(imgCopy, [approx], 0, color=(0, 0, 255), thickness=2)
+#     cv2.imshow("eee", imgCopy)
+#
+#     return res_img
+
+
 class Piro:
     def __init__(self):
         self.images = dict()
 
     def load(self, directory, numberOfImages):
         for n in range(numberOfImages):
-            self.images[n] = cv.imread(os.path.join(directory, str(n) + ".png"), cv.IMREAD_GRAYSCALE)
+            print(directory)
+            self.images[n] = cv2.imread(os.path.join(directory, str(n) + ".png"), cv2.IMREAD_GRAYSCALE)
 
     def solve(self):
         for key, im in self.images.items():
-            contours, hierarchy = cv.findContours(im, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-            imgCopy = cv.cvtColor(im.copy(), cv.COLOR_GRAY2BGR)
+            contours, hierarchy = cv2.findContours(im, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+            imgCopy = cv2.cvtColor(im.copy(), cv2.COLOR_GRAY2BGR)
 
-            epsilon = 0.0069 * cv.arcLength(contours[0], True)
-            approx = cv.approxPolyDP(contours[0], epsilon, True)
+            epsilon = 0.005 * cv2.arcLength(contours[0], True)
+            approx = cv2.approxPolyDP(contours[0], epsilon, True)
             print(len(approx))
             points = []
 
@@ -56,11 +86,14 @@ class Piro:
             angles = []
             for p in points:
                 angles.append(angle(p))
-            print(shiftAngleArray(angles))
+            shiftedAngles = shiftAngleArray(angles)
+            print(shiftedAngles)
+            base_points = shiftedAngles[0][1], shiftedAngles[0][2]
+            cv2.line(imgCopy, base_points[0], base_points[1], (0, 255, 0))
 
-            cv.drawContours(imgCopy, [approx], 0, color=(0, 0, 255), thickness=2)
-            cv.imshow("image", imgCopy)
-            cv.waitKey()
+            cv2.drawContours(imgCopy, [approx], 0, color=(0, 0, 255), thickness=2)
+            cv2.imshow("image", imgCopy)
+            cv2.waitKey()
 
 
 if __name__ == '__main__':
